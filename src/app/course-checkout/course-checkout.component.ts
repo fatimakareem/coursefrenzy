1f50321cba6032792f3c9ca2b1e43cd2552b3ac2
unknown <you@example.com>
2018-11-29 15:52:53 +0500

    wwwww

--- src/app/course-checkout/course-checkout.component.ts
+++ src/app/course-checkout/course-checkout.component.ts
@@ -46,7 +46,8 @@ export class CourseCheckoutComponent implements OnInit {
   TotalAmountForm = new FormControl('', [
     Validators.required
   ]);
-  endRequest: import("c:/Users/Brain Plow/Documents/GitHub/coursefrenzy/node_modules/rxjs/Subscription").Subscription;
+  endRequest ;
+  // import("c:/Users/Brain Plow/Documents/GitHub/coursefrenzy/node_modules/rxjs/Subscription").Subscription;
 
   constructor(private obj: UploadCoursesService,private obj_payment_service:PaymentmethodsService, private obj2: CourseCheckoutService, private global: GlobalService, private formBuilder: FormBuilder ) {
 
