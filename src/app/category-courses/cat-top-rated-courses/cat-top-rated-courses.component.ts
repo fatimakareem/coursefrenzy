23b8be8ce79e299347b8fa1a45fa286a887786e0
Haseeb6776 <41138641+Haseeb6776@users.noreply.github.com>
2019-01-01 15:51:48 +0500

    dasdad

--- src/app/category-courses/cat-top-rated-courses/cat-top-rated-courses.component.ts
+++ src/app/category-courses/cat-top-rated-courses/cat-top-rated-courses.component.ts
@@ -22,6 +22,7 @@ export class CatTopRatedCoursesComponent implements OnInit {
   public GlobalWishListCourses: any=[];
   public loaded: boolean = false;
   private category: any;
+  public slideConfig;
 
   constructor(private glb_ser: SimpleGlobal, private global: GlobalService, private nav: Router,
               public dialog: MatDialog, private obj: CoursesService) {
@@ -40,6 +41,43 @@ export class CatTopRatedCoursesComponent implements OnInit {
           this.GlobalWishListCourses = data;
         }
       });
+
+
+      this.slideConfig = {
+        infinite: false,
+        speed: 900,
+        autoplay: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        prevArrow: '<button class="leftRs">&lt;</button>',
+        nextArrow: '<button class="rightRs">&lt;</button>',
+        responsive: [
+          {
+            breakpoint: 1025,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              infinite: true
+            }
+          },
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+  ]};
+
+
+
   }
 
   ngOnInit() {
